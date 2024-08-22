b5d6e4dbe7d464bc80e0b4812f494bcfa8109186
Winterwood0118 <161268110+Winterwood0118@users.noreply.github.com>
2024-08-22 13:54:27 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 12.84 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(my_string: String): List<String> {
-        val answer = my_string.split(" ").filter{ it != " " && it != "" }
+        val answer = my_string.split(" ").filter{ it.isNotBlank() }
         return answer
     }
 }
