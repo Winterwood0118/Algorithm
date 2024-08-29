e9890a8da87a2c691a9669758e62e3ce70b6d8bc
Winterwood0118 <161268110+Winterwood0118@users.noreply.github.com>
2024-08-29 13:40:16 +0900

    [level 2] Title: 택배상자, Time: 91.39 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.kt
@@ -0,0 +1,21 @@
+import java.util.Stack
+
+class Solution {
+    fun solution(order: IntArray): Int {
+        var answer: Int = 0 // n번째 표현
+        val subContainer = Stack<Int>()
+        for(i in 1..order.size){ // 박스 만들기
+            if(i == order[answer]) answer ++ // i번 박스가 answer + 1 번째에 들어가는지 확인
+            else subContainer.push(i)
+            while(!subContainer.isEmpty()){
+                val latestBox = subContainer.pop()
+                if(latestBox == order[answer]) answer ++
+                else {
+                    subContainer.push(latestBox) 
+                    break
+                }
+            }
+        }
+        return answer
+    }
+}
