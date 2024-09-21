43d8a2c5032d04bdf41ed34ecf62df78eee007df
Winterwood0118 <161268110+Winterwood0118@users.noreply.github.com>
2024-09-21 14:32:25 +0900

    Update 두 수의 합.kt

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.kt
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.kt
@@ -1,32 +1,5 @@
-import java.math.BigInteger
-
 class Solution {
     fun solution(a: String, b: String): String {
-        /* val answer = StringBuilder()
-        
-        val longger = if( a.length >= b.length ) a else b
-        val shorter = if( a.length >= b.length ) b else a
-        
-        var remain = 0
-
-        for(i in longger.indices){
-            if(i > shorter.lastIndex) break
-            
-            var sum = longger[i].digitToInt() + shorter[i].digitToInt() + remain
-            
-            remain = 0
-            
-            if(sum >= 10) {
-                remain = sum / 10
-                sum = sum % 10
-            }
-            
-            answer.append(sum.toString())
-            
-        }
-        
-        return answer.toString().reversed()*/
-        
         return (a.toBigInteger() + b.toBigInteger()).toString()
     }
 }
