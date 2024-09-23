c63110aa3344dc8013a6e79da7df9736fc1282e8
Winterwood0118 <161268110+Winterwood0118@users.noreply.github.com>
2024-09-23 12:59:08 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.32 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(n: Int): Array<IntArray> {
+        return Array(n){ i -> 
+            IntArray(n){ j -> 
+                if(i == j) 1 else 0
+            } 
+        }
+    }
+}
