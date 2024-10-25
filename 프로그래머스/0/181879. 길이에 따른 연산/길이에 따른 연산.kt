d1c7bd5c72efddeaebfb228e85c125739196b95a
Winterwood0118 <161268110+Winterwood0118@users.noreply.github.com>
2024-10-25 12:58:22 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_list: IntArray): Int {
+        return if(num_list.size >= 11){
+            num_list.fold(0){ acc, i -> acc + i }
+        } else {
+            num_list.fold(1){ acc, i -> acc * i }
+        }
+    }
+}
